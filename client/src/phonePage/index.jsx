84bde07680cd5fa36f053d4297a244741177573f
hubiao01 <hubiao01@baijia.com>
2024-01-25 14:21:13 +0800

    fix: 二次通话bug修复

--- client/src/phonePage/index.jsx
+++ client/src/phonePage/index.jsx
@@ -93,6 +93,7 @@ export default function PhonePage() {
       trickle: false,
     })
 
+    // 在这里会触发server中的callAccepted，因此会对已经销毁的peer实例调用方法，所以在关闭时需要取消对callAccepeted的监听
     peer.on('signal', (data) => {
       socket.emit('answerCall', {
         signal: data,
@@ -109,81 +110,92 @@ export default function PhonePage() {
     connectionRef.current = peer
   }
 
+  const afterStop = () => {
+    setInCall(false)
+    setInReceving(false)
+    setCallerId('')
+    // 在销毁peer实例后需要关闭socket对callAccepted的监听
+    connectionRef.current.on('close', () => {
+      socket.off('callAccepted')
+    })
+    connectionRef.current.destroy()
+  }
+
   const stopCall = () => {
     socket.emit('stopCall', callerId)
     afterStop()
   }
 
-  const afterStop = () => {
-    setInCall(false)
-    setInReceving(false)
-    setCallerId('')
-    connectionRef.current.destroy()
-    thisRef.current = null
-    thatRef.current = null
-  }
-
   return (
     <div className="container">
       <h1 className="title">在线视频通话</h1>
-      <div className="video-container">
-        <div className="that-video">
-          <video playsInline autoPlay ref={thatRef} />
-        </div>
-        <div className="this-container">
-          <div className="this-video">
-            <video playsInline muted autoPlay ref={thisRef} />
+      <div className="content">
+        <div className="video-container">
+          <div className="that-video">
+            {inCall && <video playsInline autoPlay ref={thatRef} />}
           </div>
-          <div className="input-pannel">
-            <Input
-              placeholder="请输入您的昵称"
-              value={thisName}
-              onChange={(e) => setThisName(e.target.value)}
-            />
-            <CopyToClipboard text={thisId} className="clip">
-              <Button
-                icon={<CopyOutlined style={{ fontSize: '15px' }} />}
-                type="primary"
-              >
-                我的通话ID
-              </Button>
-            </CopyToClipboard>
-            <Input
-              placeholder="请输入需要拨打的ID"
-              value={thatId}
-              onChange={(e) => setThatId(e.target.value)}
-            />
-            <div>
-              {inCall ? (
-                <Button type="primary" danger onClick={stopCall}>
-                  结束通话
-                </Button>
-              ) : (
-                <div
-                  className="phone-container"
-                  onClick={() => makePhoneCall(thatId)}
+          <div className="this-container">
+            <div className="this-video">
+              <video playsInline muted autoPlay ref={thisRef} />
+            </div>
+            <div className="input-pannel">
+              <Input
+                placeholder="请输入您的昵称"
+                value={thisName}
+                onChange={(e) => setThisName(e.target.value)}
+              />
+              <CopyToClipboard text={thisId} className="clip">
+                <Button
+                  icon={<CopyOutlined style={{ fontSize: '15px' }} />}
+                  type="primary"
                 >
-                  <PhoneFilled
-                    style={{
-                      fontSize: '28px',
-                      color: '#7DAEAC',
-                      cursor: 'pointer',
-                    }}
-                  />
-                </div>
-              )}
+                  我的通话ID
+                </Button>
+              </CopyToClipboard>
+              <Input
+                placeholder="请输入需要拨打的ID"
+                value={thatId}
+                onChange={(e) => setThatId(e.target.value)}
+              />
+              <div>
+                {inCall ? (
+                  <Button type="primary" danger onClick={stopCall}>
+                    结束通话
+                  </Button>
+                ) : (
+                  <div
+                    className="phone-container"
+                    onClick={() => makePhoneCall(thatId)}
+                  >
+                    <PhoneFilled
+                      style={{
+                        fontSize: '28px',
+                        color: '#7DAEAC',
+                        cursor: 'pointer',
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
+        {inReceving && (
+          <div className="be-calling">
+            <h2>{thatName}正在呼叫...</h2>
+            <Button
+              type="primary"
+              onClick={answerCall}
+              style={{ marginRight: '10px' }}
+            >
+              同意接听
+            </Button>
+            <Button type="primary" danger onClick={stopCall}>
+              拒绝通话
+            </Button>
+          </div>
+        )}
       </div>
-      {inReceving && (
-        <div className="be-calling">
-          <h2>{thatName}正在呼叫...</h2>
-          <Button type="primary" onClick={answerCall}>
-            同意接听
-          </Button>
-        </div>
-      )}
     </div>
   )
 }
